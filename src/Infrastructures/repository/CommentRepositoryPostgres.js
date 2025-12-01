6679b1394f6d5725d03782eabc7f07c98066d5ca
ajayfaul <muhammadfajarm@gmail.com>
2025-12-01 20:54:42 +0700

    feat: implement add and delete comment functionality

---
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -0,0 +1,70 @@
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
+const AddedComment = require('../../Domains/comments/entities/AddedComment');
+const CommentRepository = require('../../Domains/comments/CommentRepository');
+
+class CommentRepositoryPostgres extends CommentRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addComment(threadId, userId, newComment) {
+    const { content } = newComment;
+    const id = `comment-${this._idGenerator()}`;
+    const date = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6) RETURNING id, content, owner',
+      values: [id, content, date, threadId, userId, false],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new AddedComment({ ...result.rows[0] });
+  }
+
+  async deleteComment(commentId) {
+    const query = {
+      text: 'UPDATE comments SET is_delete = true WHERE id = $1',
+      values: [commentId],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async verifyCommentOwner(commentId, userId) {
+    const query = {
+      text: 'SELECT owner FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('komentar tidak ditemukan');
+    }
+
+    const comment = result.rows[0];
+
+    if (comment.owner !== userId) {
+      throw new AuthorizationError('anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async checkCommentAvailability(commentId) {
+    const query = {
+      text: 'SELECT id FROM comments WHERE id = $1',
+      values: [commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('komentar tidak ditemukan');
+    }
+  }
+}
+
+module.exports = CommentRepositoryPostgres;
